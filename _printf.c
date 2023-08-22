3e4ce0c9f31b8f0fab7019ea369ff53497b2f01b
FatemaNunow <fatema14384@gmail.com>
2023-08-22 04:36:28 -0700

    add all files

--- _printf.c
+++ _printf.c
@@ -51,7 +51,6 @@ int _printf(const char *format, ...)
 
 	return (printed_chars);
 }
-
 /**
  * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
--- functions1.c
+++ functions1.c
@@ -94,7 +94,6 @@ int print_hexadecimal(va_list types, char buffer[],
 	return (print_hexa(types, "0123456789abcdef", buffer,
 		flags, 'x', width, precision, size));
 }
-
 /************* PRINT UNSIGNED NUMBER IN UPPER HEXADECIMAL **************/
 /**
  * print_hexa_upper - Prints an unsigned number in upper hexadecimal notation
